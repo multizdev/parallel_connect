ae27d5e6101d86121f004922ff2a8d76f287231f
multidevs <multiz.dev@gmail.com>
2024-11-26 20:40:08 +0500

    Refactor CSS for readability and update lockfile quotes
    Improving CSS background property for better readability by reformatting and aligning the values. Additionally, standardize the quotes in the pnpm lockfile for consistent adherence to coding conventions.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['nftmedia.parallelnft.com'],
-  }
+    domains: ["nftmedia.parallelnft.com"],
+  },
 };
 
 export default nextConfig;
